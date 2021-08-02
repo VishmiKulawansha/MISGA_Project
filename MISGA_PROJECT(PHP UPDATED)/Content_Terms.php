5ec34d08e58f8504a4fffbedd5300cb8a96c3ad8
EASHAN_WIJESINGHA <87780134+EashanWI@users.noreply.github.com>
2021-08-02 20:14:49 +0530

    Example

--- MISGA_PROJECT(PHP UPDATED)/Content_Terms.php
+++ MISGA_PROJECT(PHP UPDATED)/Content_Terms.php
@@ -1,4 +1,4 @@
-<section id="terms" class="container">
+    <section id="terms" class="container">
         <h3>Introduction</h3>
         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
 
