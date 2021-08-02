e8937dc1bbc123d3bf0b9dd0fce9b208aee8e892
Lakmali <87770503+LakmaliThilakarathna@users.noreply.github.com>
2021-08-02 23:49:29 +0530

    update portfolio

--- MISGA_PROJECT(PHP UPDATED)/Content_Portfolio.php
+++ MISGA_PROJECT(PHP UPDATED)/Content_Portfolio.php
@@ -1,34 +1,37 @@
-<section id="portfolio" class="container">
+
+    <section id="portfolio" class="container">
         <ul class="portfolio-filter">
-            <li><a class="btn btn-default active" href="#" data-filter="*">All</a></li>
-            <li><a class="btn btn-default" href="#" data-filter=".bootstrap">Bootstrap</a></li>
-            <li><a class="btn btn-default" href="#" data-filter=".html">HTML</a></li>
-            <li><a class="btn btn-default" href="#" data-filter=".wordpress">Wordpress</a></li>
+            <li><a class="btn btn-default" href="#" data-filter="">All</a></li>
+            <li><a class="btn btn-default" href="#" data-filter=".bootstrap">Tourism and Leisure</a></li>
+            <li><a class="btn btn-default" href="#" data-filter=".html">Promotional Events</a></li>
+            <li><a class="btn btn-default" href="#" data-filter=".wordpress">Meeting Organization</a></li>
         </ul><!--/#portfolio-filter-->
 
+<div>
+  <body style= "background: url(https://wallpapertag.com/wallpaper/full/a/b/8/416537-navy-blue-backgrounds-1920x1200-image.jpg)">
+ 
         <ul class="portfolio-items col-3">
-            <li class="portfolio-item apps">
+           
                 <div class="item-inner">
                     <img src="images/portfolio/thumb/item1.jpg" alt="">
-                    <h5>Lorem ipsum dolor sit amet</h5>
                     <div class="overlay">
-                        <a class="preview btn btn-danger" href="images/portfolio/full/item1.jpg" rel="prettyPhoto"><i class="icon-eye-open"></i></a>             
+                      
                     </div>           
                 </div>           
             </li><!--/.portfolio-item-->
             <li class="portfolio-item joomla bootstrap">
                 <div class="item-inner">
                     <img src="images/portfolio/thumb/item2.jpg" alt="">
-                    <h5>Lorem ipsum dolor sit amet</h5>
+                    <h5>Kitulgala Water Rafting</h5>
                     <div class="overlay">
                         <a class="preview btn btn-danger" href="images/portfolio/full/item2.jpg" rel="prettyPhoto"><i class="icon-eye-open"></i></a>              
                     </div>           
                 </div>           
             </li><!--/.portfolio-item-->
-            <li class="portfolio-item bootstrap wordpress">
+            <li class="portfolio-item joomla bootstrap">
                 <div class="item-inner">
                     <img src="images/portfolio/thumb/item3.jpg" alt="">
-                    <h5>Lorem ipsum dolor sit amet</h5>
+                    <h5> Wildlife Tours</h5>
                     <div class="overlay">
                         <a class="preview btn btn-danger" href="images/portfolio/full/item3.jpg" rel="prettyPhoto"><i class="icon-eye-open"></i></a>        
                     </div>           
@@ -37,7 +40,7 @@
             <li class="portfolio-item joomla wordpress apps">
                 <div class="item-inner">
                     <img src="images/portfolio/thumb/item4.jpg" alt="">
-                    <h5>Lorem ipsum dolor sit amet</h5>
+                    <h5>Annual meeting an industries group</h5>
                     <div class="overlay">
                         <a class="preview btn btn-danger" href="images/portfolio/full/item4.jpg" rel="prettyPhoto"><i class="icon-eye-open"></i></a>          
                     </div>           
@@ -46,20 +49,21 @@
             <li class="portfolio-item joomla html">
                 <div class="item-inner">
                     <img src="images/portfolio/thumb/item5.jpg" alt="">
-                    <h5>Lorem ipsum dolor sit amet</h5>
+                    <h5> Promotional event for Traditional Foods</h5>
                     <div class="overlay">
                         <a class="preview btn btn-danger" href="images/portfolio/full/item5.jpg" rel="prettyPhoto"><i class="icon-eye-open"></i></a>          
                     </div>    
                 </div>       
             </li><!--/.portfolio-item-->
-            <li class="portfolio-item wordpress html">
+            <li class="portfolio-item joomla html">
                 <div class="item-inner">
                     <img src="images/portfolio/thumb/item6.jpg" alt="">
-                    <h5>Lorem ipsum dolor sit amet</h5>
+                    <h5> Promotional event for Arts and Crafts </h5>
                     <div class="overlay">
                         <a class="preview btn btn-danger" href="images/portfolio/full/item6.jpg" rel="prettyPhoto"><i class="icon-eye-open"></i></a>           
                     </div>           
                 </div>           
             </li><!--/.portfolio-item-->
         </ul>
+    </div>
     </section><!--/#portfolio-->
