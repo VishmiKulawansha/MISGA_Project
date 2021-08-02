7604232f06a4466f818c0d0b12a1138920fa7ee0
NipunikaShashini <87492344+NipunikaShashini@users.noreply.github.com>
2021-08-02 21:34:08 +0530

    updated as PHP

--- MISGA_PROJECT(PHP UPDATED)/Content_About_Us.php
+++ MISGA_PROJECT(PHP UPDATED)/Content_About_Us.php
@@ -1,11 +1,16 @@
+<!--First(1) Div-->
+<!--First(1) Div-->
+<!--First(1) Div-->
+<!--First(1) Div-->
+
 <section id="about-us" class="container">
         <div class="row">
             <div class="col-sm-6">
-                <h2>What we are</h2>
+                <h2><b>What we are</b></h2>
                 <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
             </div><!--/.col-sm-6-->
             <div class="col-sm-6">
-                <h2>Our Skills</h2>
+                <h2><b>Our Skills</b></h2>
                 <div>
                     <div class="progress">
                         <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
@@ -14,12 +19,12 @@
                     </div>
                     <div class="progress">
                         <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">
-                            <span>Wordpress</span>
+                            <span>Java</span>
                         </div>
                     </div>
                     <div class="progress">
                         <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
-                            <span>Joomla</span>
+                            <span>Mysql</span>
                         </div>
                     </div>
                     <div class="progress">
@@ -36,4 +41,49 @@
             </div><!--/.col-sm-6-->
         </div><!--/.row-->
 
+
+
+
+
+<!--second(2) Div-->
+<!--second(2) Div-->
+<!--second(2) Div-->
+<!--second(2) Div-->
+
+         <div class="row">
+            <div class="col-sm-6">
+                <h3><b>Our Services Include,</b></h3>
+                    <ul type="square">
+                        <li>Mobile App Development</li> 
+                        <li>Web Development</li>
+                        <li>Social Media Marketing</li>
+                        <li>E-Commerce Solutions</li>
+                        <li>Media Planning</li>
+                        <li>Search Engine Optimizing</li>
+                        <li>Customize Software</li>
+                        <li>Advertising</li>
+                        <li>Branding</li>
+                    </ul>
+            </div>
         
+
+            <div class="col-sm-6">
+                    <h3><b>Our Technologies,</b></h3>
+                            <ul type="square">
+                                <li>We are passionate about web technologies and constantly keep
+                                    an eye out for new technologies that make us more efficient. We favour open source technologies such as PHP and MySQL on Linux. We extensively use Zend Framework, ExtJS, jQuery and AJAX. With certain projects with high load we have also successfully increased throughput with the Cloud computing, Node.js and HTML5. We are an independent agency so we are happy to customise any product that you recommend. All our clients have very different requirements and we try to be as flexible as possible to offer the most suitable solution.</li> 
+                                
+                            </ul>
+             </div>
+        </div></br>
+
+<style>
+.pold{border-style: inset;}
+</style>
+</br><table width="1200">
+            <tr>
+                <td width="500"><p><iframe width="100%" height="320" src="https://www.youtube.com/embed/lW4Wk3hoTCw?Rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p></td>
+                <td width="50"></td>
+                <td width ="500"><p class="pold"><center><h3><b>Our Mission</b></h3></center></br> <center>“To be Earth’s most customer-centric company, where customers can find and discover anything they might want to buy online, and endeavors to offer its customers the lowest possible prices.”</center></td></p>
+            </tr>
+        </table></section></br>
--- MISGA_PROJECT(PHP UPDATED)/Team_About_Us.php
+++ MISGA_PROJECT(PHP UPDATED)/Team_About_Us.php
@@ -1,3 +1,8 @@
+<!--Third (3) Div-->
+<!--Third (3) Div-->
+<!--Third (3) Div-->
+<!--Third (3) Div-->
+
 <div class="gap"></div>
         <h1 class="center">Meet the Team</h1>
         <p class="lead center">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
@@ -6,35 +11,36 @@
         <div id="meet-the-team" class="row">
             <div class="col-md-3 col-xs-6">
                 <div class="center">
-                    <p><img class="img-responsive img-thumbnail img-circle" src="images/team-member.jpg" alt="" ></p>
-                    <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
+                    <p><img class="img-responsive img-thumbnail img-circle" src="images/ads/girl1.jpg" alt="" ></p>
+                    <h5> W A S N Fernando<small class="designation muted">Senior Vice President</small></h5>
                     <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
-                    <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
+                    <a class="btn btn-social btn-facebook" href="https://www.facebook.com/university.colombo/"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="https://www.facebook.com/university.colombo/"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="https://www.facebook.com/university.colombo/"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="https://www.facebook.com/university.colombo/"><i class="icon-linkedin"></i></a>
+         
                 </div>
             </div>
 
             <div class="col-md-3 col-xs-6">
                 <div class="center">
-                    <p><img class="img-responsive img-thumbnail img-circle" src="images/team-member.jpg" alt="" ></p>
-                    <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
+                    <p><img class="img-responsive img-thumbnail img-circle" src="images/ads/boy1.jpg" alt="" ></p>
+                    <h5>N A T David<small class="designation muted">Senior Vice President</small></h5>
                     <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
-                    <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
+                    <a class="btn btn-social btn-facebook" href="https://www.facebook.com/university.colombo/"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="https://www.facebook.com/university.colombo/"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="https://www.facebook.com/university.colombo/"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="https://www.facebook.com/university.colombo/"><i class="icon-linkedin"></i></a>
                 </div>
             </div>        
             <div class="col-md-3 col-xs-6">
                 <div class="center">
-                    <p><img class="img-responsive img-thumbnail img-circle" src="images/team-member.jpg" alt="" ></p>
-                    <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
+                    <p><img class="img-responsive img-thumbnail img-circle" src="images/ads/girl2.jpg" alt="" ></p>
+                    <h5>K J I Omega<small class="designation muted">Senior Vice President</small></h5>
                     <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
-                    <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
+                    <a class="btn btn-social btn-facebook" href="https://www.facebook.com/university.colombo/"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="https://www.facebook.com/university.colombo/"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="https://www.facebook.com/university.colombo/"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="https://www.facebook.com/university.colombo/"><i class="icon-linkedin"></i></a>
                 </div>
             </div>        
             <div class="col-md-3 col-xs-6">
                 <div class="center">
-                    <p><img class="img-responsive img-thumbnail img-circle" src="images/team-member.jpg" alt="" ></p>
-                    <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
+                    <p><img class="img-responsive img-thumbnail img-circle" src="images/ads/boy2.jpg" alt="" ></p>
+                    <h5>H K Robbin<small class="designation muted">Senior Vice President</small></h5>
                     <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
-                    <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
+                    <a class="btn btn-social btn-facebook" href="https://www.facebook.com/university.colombo/"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="https://www.facebook.com/university.colombo/"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="https://www.facebook.com/university.colombo/"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="https://www.facebook.com/university.colombo/"><i class="icon-linkedin"></i></a>
                 </div>
             </div>
         </div><!--/#meet-the-team-->
