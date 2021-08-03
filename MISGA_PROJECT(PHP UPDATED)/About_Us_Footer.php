b813af28c586f8e03188ed262db87737599225a8
Nipunika W.A.S <87492344+NipunikaShashini@users.noreply.github.com>
2021-08-03 13:12:55 +0530

    Update About_Us_Footer.php

--- MISGA_PROJECT(PHP UPDATED)/About_Us_Footer.php
+++ MISGA_PROJECT(PHP UPDATED)/About_Us_Footer.php
@@ -1,88 +1,60 @@
 <section style="background-image: url('images/background image/bg.jpg'); background-attachment: fixed;">
     <section id="bottom" class="asdf">
         
+        <!--Footer-->
+<section id="bottom" class="wet-asphalt">
         <div class="container">
             <div class="row">
                 <div class="col-md-3 col-sm-6">
-                    <h3></h3>
                     <h4>About Us</h4>
-                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</i></p>
                     <p>Pellentesque habitant morbi tristique senectus.</p>
                 </div><!--/.col-md-3-->
 
                 <div class="col-md-3 col-sm-6">
-                    <h3></h3>
                     <h4>Company</h4>
                     <div>
                         <ul class="arrow">
-                            <li><a href="#">Company Overview</a></li>
-                            <li><a href="#">Meet The Team</a></li>
-                            <li><a href="#">Our Awesome Partners</a></li>
-                            <li><a href="#">Our Services</a></li>
-                            <li><a href="#">Frequently Asked Questions</a></li>
-                            <li><a href="#">Conatct Us</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Terms of Use</a></li>
-                            <li><a href="#">Copyright</a></li>
+                            <li><a href="1_INDEX.php">The Company</a></li>
+                            <li><a href="2_ABOUT_US.php">Our Team</a></li>
+                            <li><a href="3_SERVICES.php">Our Services</a></li>
+                            <li><a href="5_CONTACT_US.php">Conatct Us</a></li>
+                            <li><a href="6_PRIVACY.php">Privacy Policy</a></li>
+                            <li><a href="6_TERMS.php">Terms of Use</a></li>
+                           
                         </ul>
                     </div>
                 </div><!--/.col-md-3-->
 
                 <div class="col-md-3 col-sm-6">
-                    <h3></h3>
-                    <h4>Latest Blog</h4>
-                    <div>
-                        <div class="media">
-                            <div class="pull-left">
-                                <img src="images/blog/thumb1.jpg" alt="">
-                            </div>
-                            <div class="media-body">
-                                <span class="media-heading"><a href="#">Pellentesque habitant morbi tristique senectus</a></span>
-                                <small class="muted">Posted 17 Aug 2013</small>
-                            </div>
+                    <h4>Other Links</h4>
+                        <div>
+                            <ul class="arrow">
+                                <li><a href="https://www.srilankan.com/en_uk/lk">Sri Lankan Airlens</a></li>
+                                <li><a href="https://www.srilankancatering.com/">Sri Lankan Catering</a></li>
+                                <li><a href="https://www.srilankanholidays.com/">Sri Lankan Holidays</a></li>
+                                <li><a href="https://www.srilankan.com/en_uk/flying-with-us/airtaxi">Sri Lankan Air Taxi</a></li>
+                                <li><a href="https://www.srilankan.com/ground-handling/welcome.htm">Ground Hadling</a></li>
+                                <li><a href="https://www.srilankanaviationcollege.com/">Training</a></li>
+                           
+                            </ul>
                         </div>
-                        <div class="media">
-                            <div class="pull-left">
-                                <img src="images/blog/thumb2.jpg" alt="">
-                            </div>
-                            <div class="media-body">
-                                <span class="media-heading"><a href="#">Pellentesque habitant morbi tristique senectus</a></span>
-                                <small class="muted">Posted 13 Sep 2013</small>
-                            </div>
-                        </div>
-                        <div class="media">
-                            <div class="pull-left">
-                                <img src="images/blog/thumb3.jpg" alt="">
-                            </div>
-                            <div class="media-body">
-                                <span class="media-heading"><a href="#">Pellentesque habitant morbi tristique senectus</a></span>
-                                <small class="muted">Posted 11 Jul 2013</small>
-                            </div>
-                            <h3></h3>
-                        </div>
-                    </div>  
-                </div><!--/.col-md-3-->
+                </div>
 
-                <div class="col-md-3 col-sm-6">
-                    <h3></h3>
-                    <h4>Address</h4>
-                    <address>
-                        <strong>Twitter, Inc.</strong><br>
-                        795 Folsom Ave, Suite 600<br>
-                        San Francisco, CA 94107<br>
-                        <abbr title="Phone">P:</abbr> (123) 456-7890
-                    </address>
-                    <h4>Newsletter</h4>
+
+                <div> 
+                 <h4>Newsletter</h4>
                     <form role="form">
                         <div class="input-group">
                             <input type="text" class="form-control" autocomplete="off" placeholder="Enter your email">
                             <span class="input-group-btn">
-                                <button class="btn btn-danger" type="button" style="background-color: rgb(13, 146, 255);">Go!</button>
+                                <button class="btn btn-danger" type="button">Go!</button>
                             </span>
                         </div>
                     </form>
                 </div> <!--/.col-md-3-->
             </div>
         </div>
+    </section><!--/#bottom-->
     </section><!--/#bottom-->
     </section>
