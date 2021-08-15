4de3721cdb6e7943e2ec090bff8a07b0b5acd0ca
VIshmi Kulawansha <87770469+VishmiKulawansha@users.noreply.github.com>
2021-08-16 02:30:27 +0530

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Freshcery | Groceries Organic Store</title>
+    <title>Krish Villa | Groceries Organic Store</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
